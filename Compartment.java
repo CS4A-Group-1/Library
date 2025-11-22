816d2548f80a9fc64f9cb9397c008838adc87aa4
Kyle Larson <kyle.larson0108@gmail.com>
2025-11-21 19:36:47 -0800

    Compartment.java, Still needs toString()

---
+++ Compartment.java
@@ -0,0 +1,71 @@
+import java.io.Serializable;
+import java.util.Date;
+
+public class Compartment implements Serializable {
+
+    // Due Date Constant
+    private final long DUE_DATE_OFFSET = 14L * 24 * 60 * 60 * 1000; // set to 14 days
+
+    private Item item;
+    private boolean isCheckedOut;
+    private String currentBorrower;
+    private Date checkoutDate;
+
+    // You must create a compartment with a valid item in it
+    public Compartment(Item i) {
+        if (i == null)
+            throw new IllegalStateException("Item cannot be null.");
+
+        this.isCheckedOut = false;
+        this.currentBorrower = null;
+        this.checkoutDate = null;
+        this.item = i;
+    }
+
+    // If item is already checked out, return false.
+    public boolean checkOut(String borrower) {
+        if (isCheckedOut) return false;
+
+        this.isCheckedOut = true;
+        this.currentBorrower = borrower;
+        this.checkoutDate = new Date();
+
+        return this.isCheckedOut;
+    }
+
+    // If item is already checked in, return false
+    public boolean checkIn() {
+        if (!isCheckedOut) return false;
+
+        this.isCheckedOut = false;
+        this.currentBorrower = null;
+        this.checkoutDate = null;
+
+        return this.isCheckedOut;
+    }
+
+    // GETTERS
+    public Item getItem() {
+        return item;
+    }
+
+    public boolean getIsCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public String getCurrentBorrower() {
+        return currentBorrower;
+    }
+
+    public Date getCheckoutDate() {
+        return checkoutDate;
+    }
+
+    public Date getDueDate() {
+        if (checkoutDate == null) return null;
+
+        return new Date(checkoutDate.getTime() + DUE_DATE_OFFSET);
+    }
+
+    //TODO toString()
+}
