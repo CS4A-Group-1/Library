915081af210a3593792c00a943371719111e6977
Woofels <115676080+Woofels@users.noreply.github.com>
2025-11-21 18:48:28 -0800

    Add Shelf class Skeleton

---
+++ Shelf.java
@@ -0,0 +1,25 @@
+public class Shelf {
+    /*private String section;
+    private ArrayList<Compartment> compartments;
+
+    public Shelf() { }
+
+    public Shelf(ArrayList<Compartment> c) { }
+
+    public void setSection(String s) { }
+
+    public String getSection() { }
+
+    public void setCompartment(Compartment c, int i) { }
+
+    public Compartment getCompartment(int i) { }
+
+    public ArrayList<Compartment> getCompartments() { }
+
+    public boolean addCompartment(Item item) { }
+
+    public void swapCompartments(int i, int j) { }
+
+    @Override
+    public String toString() { }*/
+}
